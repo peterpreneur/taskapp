122aa2ccfdd65bf4ce4842876068e18eb868ae75
Peter S <peterpreneur@gmail.com>
2026-06-08 00:01:39 -0500

    repo

--- src/main/java/com/peterpreneur/taskapp/domain/dto/CreateTaskRequestDto.java
+++ src/main/java/com/peterpreneur/taskapp/domain/dto/CreateTaskRequestDto.java
@@ -10,7 +10,10 @@ import org.hibernate.validator.constraints.Length;
 
 import com.peterpreneur.taskapp.domain.entity.TaskPriority;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 /**
  *
@@ -21,13 +24,21 @@ public record CreateTaskRequestDto(
         @Length(max = 255)
         String title,
         @Length(max = 1000, message = ERROR_MESSAGE_DESCRIPTION_LENGTH)
+        @Nullable
         String description,
+        @FutureOrPresent(message = ERROR_MESSAGE_DUE_DATE_FUTURE)
+        @Nullable
         LocalDate dueDate,
+        @NotNull(message = ERROR_MESSAGE_PRIORITY)
         TaskPriority priority
         ) {
 
     private static final String ERROR_MESSAGE_TITLE_LENGTH = "Title must be at most 255 characters long";
 
     private static final String ERROR_MESSAGE_DESCRIPTION_LENGTH = "Description must be at most 255 characters long";
+
+    private static final String ERROR_MESSAGE_DUE_DATE_FUTURE = "Due date must be in the present or future";
+
+    private static final String ERROR_MESSAGE_PRIORITY = "Priority must not be null";
 
 }
